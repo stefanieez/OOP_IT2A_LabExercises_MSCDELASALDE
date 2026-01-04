e52093c8801f75e728f3beccce56c60766698213
stefanieez <stefaniedelasalde@gmail.com>
2026-01-04 23:02:05 +0800

    Add Product class with name and price attributes

---
+++ EXER5/PROJECT RELATED/AgriConnect/Product.java
@@ -0,0 +1,19 @@
+package AgriConnect;
+
+public class Product {
+    private String productName;
+    private double price;
+
+    public Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
